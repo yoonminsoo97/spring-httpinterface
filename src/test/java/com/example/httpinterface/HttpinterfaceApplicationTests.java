7715bd7793e83ec1c51288bfe8d077784e2adb0c
yoonminsoo <yoonminwater@gmail.com>
2025-04-07 08:06:21 +0900

    initial commit

---
+++ src/test/java/com/example/httpinterface/HttpinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpinterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
